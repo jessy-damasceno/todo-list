03e4ef3521eb7810b23710b763d24f7780865f00
Jessy Damasceno <jessy_bas@hotmail.com>
2022-03-22 18:44:45 -0300

    add reqs 5, 6, 7, 8

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 const inputText = document.querySelector('#texto-tarefa');
 const btnTarefa = document.querySelector('#criar-tarefa');
+const list = document.querySelector('#lista-tarefas');
+
+function addItemList() {
+  const itemList = document.createElement('li');
+  itemList.innerText = inputText.value;
+  list.appendChild(itemList);
+  inputText.value = '';
+}
+
+btnTarefa.addEventListener('click', addItemList);
+
+function selectColor(e) {
+  const itemListElements = document.getElementsByTagName('li');
+  for (let i = 0; i < itemListElements.length; i += 1) {
+    itemListElements[i].classList.remove('selected');
+  }
+  e.target.classList.add('selected');
+  list.classList.remove('selected');
+}
+
+list.addEventListener('click', selectColor);
